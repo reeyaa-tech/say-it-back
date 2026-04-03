c41b590a74e92776825df5abeeae697813926867
Reeya <reeyaa0812@gmail.com>
2026-04-03 14:04:34 +0530

    make changes

--- api/analyze.js
+++ api/analyze.js
@@ -5,7 +5,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 console.log("Key loaded:", process.env.GEMINI_API_KEY ? "Yes" : "No");
-cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
